0e4d9425ebad7d8053eb04561ad44ff05c1bc4f9
JohanSannier <johan.sanniercaffier@gmail.com>
2022-06-03 12:09:32 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
-// import Footer from "./components/footer";
+import Footer from "./components/footer";
 
 function App(props) {
   return (
     <div className="app">
       <Navbar />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./index.css";
+import zen from "../../assets/zen.png";
+import swim from "../../assets/swim.png";
+import bike from "../../assets/bike.png";
+import weight from "../../assets/weight.png";
 
 function index(props) {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer>
+      <div className="footer-icons">
+        <img src={zen} alt="Icone de méditation" />
+        <img src={swim} alt="Icone de natation" />
+        <img src={bike} alt="Icone de vélo" />
+        <img src={weight} alt="Icone de poids" />
+      </div>
+      <span>Copiryght, SportSee 2020</span>
+    </footer>
   );
 }
 
