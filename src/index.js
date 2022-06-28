20e765daeabc4e11c9a8532511e077cbe6aa08d6
JohanSannier <johan.sanniercaffier@gmail.com>
2022-06-28 10:57:29 +0200

    Correction route error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Error from "./pages/Error";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Error from "./pages/Error";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,7 +5,7 @@ function Error(props) {
   return (
     <div className="error-page">
       Cette page n'existe pas.
-      <Link to="/user/12">Retourner à l'accueil</Link>
+      <Link to="/">Retourner à l'accueil</Link>
     </div>
   );
 }
